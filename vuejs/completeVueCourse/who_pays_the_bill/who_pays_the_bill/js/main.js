facdfdddcdd608f46f1ecb120487d418da845b91
Camilo Romero <camilo@pop-os.localdomain>
2020-07-15 19:22:38 -0500

    First Vue project

---
+++ vuejs/completeVueCourse/who_pays_the_bill/who_pays_the_bill/js/main.js
@@ -0,0 +1,45 @@
+new Vue({
+	el: '#app',
+	data() {
+		return {
+			state: true,
+			inputName: '',
+			names: [],
+			showError: false,
+			loserName: '',
+		};
+	},
+	methods: {
+		addName() {
+			if (this.validateName(this.inputName)) {
+				this.showError = false;
+				this.names = [...this.names, this.inputName];
+				this.inputName = '';
+			} else {
+				this.showError = true;
+			}
+		},
+		validateName(value) {
+			if (value !== '') {
+				return true;
+			}
+			return false;
+		},
+		checkLoser() {
+			this.loserName = this.names[
+				Math.floor(Math.random() * this.names.length)
+			];
+			this.changeAppWindow();
+		},
+		changeAppWindow() {
+			this.state = !this.state;
+		},
+		resetApp() {
+			this.changeAppWindow();
+			this.names = [];
+		},
+		removeName(index) {
+			this.names.splice(index, 1);
+		},
+	},
+});

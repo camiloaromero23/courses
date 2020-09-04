415442d02007d3265f34c781f11e40631fe3ac6e
Camilo Romero <camiloaromero23@gmail.com>
2020-09-03 19:12:13 -0500

    Finished Mongoose module

---
+++ nodeJS/basicNodeExpressServer/models/order.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const orderSchema = mongoose.Schema({
+	products: [
+		{
+			product: { type: Object, required: true },
+			quantity: { type: Number, required: true },
+		},
+	],
+	user: {
+		name: { type: String, required: true },
+		userId: {
+			type: mongoose.Schema.Types.ObjectID,
+			required: true,
+			ref: 'User',
+		},
+	},
+});
+
+module.exports = mongoose.model('Order', orderSchema, 'orders');

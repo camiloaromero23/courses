3e6e30ea6d9a95bf9dbcb2bae7e63b8f690ae9e5
Camilo Romero <camiloaromero23@gmail.com>
2020-10-05 23:22:53 -0500

    Finished first web scraping project

---
+++ python/theCompletePythonCourse/scraping_quotes/app.py
@@ -0,0 +1,13 @@
+import requests
+
+from pages.quotes_page import QuotesPage
+
+page_content = requests.get('http://quotes.toscrape.com').content
+page = QuotesPage(page_content)
+
+for quote in page.quotes:
+    print(
+        f'''Quote:
+    Author: {quote.author},
+    Content: {quote.content},
+    Tags: {quote.tags}''')

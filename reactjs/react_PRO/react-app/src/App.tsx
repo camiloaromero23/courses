82332671dd0fff2fa541fddde244b36e1efed96c
Camilo Romero <camiloaromero23@gmail.com>
2021-11-22 19:00:06 -0500

    Finished project initialization

---
+++ reactjs/react_PRO/react-app/src/App.tsx
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+
+import { Navigation } from './routes/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;

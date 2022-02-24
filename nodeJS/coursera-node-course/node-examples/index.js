db33afed43652f364924ebb9a136be5e4e4bfa1b
Camilo Romero <camiloaromero23@gmail.com>
2022-02-24 16:52:05 -0500

    Simple node module

--- nodeJS/coursera-node-course/node-examples/index.js
+++ nodeJS/coursera-node-course/node-examples/index.js
@@ -1,16 +1,12 @@
-const rect = {
-  perimeter: ( x, y ) => 2 * ( x + y ),
-  area: ( x, y ) => x * y
-}
-
+import { area, perimeter } from './rectangle.js'
 const solveRect = ( l, b ) => {
   console.log( `Solving for rectangle with l = ${l} and b = ${b}` );
 
   if ( l <= 0 || b <= 0 ) {
     console.log( `Rectangle dimensions should be greater than zero: l = ${l} and b = ${b}` )
   } else {
-    console.log( `The area of the rectangle is ${rect.area( l, b )}` )
-    console.log( `The perimeter of the rectangle is ${rect.perimeter( l, b )}` )
+    console.log( `The area of the rectangle is ${area( l, b )}` )
+    console.log( `The perimeter of the rectangle is ${perimeter( l, b )}` )
   }
 }
 
---
+++ nodeJS/coursera-node-course/node-examples/rectangle.js
@@ -0,0 +1,2 @@
+export const perimeter = ( x, y ) => 2 * ( x + y )
+export const area = ( x, y ) => x * y

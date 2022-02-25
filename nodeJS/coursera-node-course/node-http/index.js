9414cac4509cea0d2e0fa9e3c8bf73385a54dcb5
Camilo Romero <camiloaromero23@gmail.com>
2022-02-25 10:25:57 -0500

    Node HTTP Example 2

--- nodeJS/coursera-node-course/node-http/index.js
+++ nodeJS/coursera-node-course/node-http/index.js
@@ -1,13 +1,50 @@
 import http from 'http';
+import fs from 'fs'
+import path from 'path';
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer( ( req, res ) => {
-  console.log( req.headers );
-  res.statusCode = 200;
-  res.setHeader( 'Content-Type', 'text/html' );
-  res.end( '<html><body><h1>Halooo!</h1></body></html>' );
+  console.log( `Request for ${req.url} by method ${req.method}` );
+
+  if ( req.method == 'GET' ) {
+    const fileUrl = req.url === '/' ? '/index.html' : req.url;
+
+    let filePath = path.resolve( `./public${fileUrl}` )
+    const fileExt = path.extname( filePath )
+    if ( fileExt === '.html' ) {
+      fs.access( filePath, fs.constants.R_OK, ( err ) => {
+        if ( err ) {
+          res.statusCode = 404;
+          // res.setHeader()
+          res.setHeader( 'Content-Type', 'text/html' );
+          res.end( `<html><body><h1>Error 404: ${filePath} not found</h1></body></html>` );
+
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader( 'Content-Type', 'text/html' );
+        fs.createReadStream( filePath ).pipe( res )
+      } )
+    } else {
+      res.statusCode = 404;
+      // res.setHeader()
+      res.setHeader( 'Content-Type', 'text/html' );
+      res.end( `<html><body><h1>Error 404: ${filePath} not an HTML file</h1></body></html>` );
+
+      return;
+    }
+  } else {
+
+    res.statusCode = 404;
+    // res.setHeader()
+    res.setHeader( 'Content-Type', 'text/html' );
+    res.end( `<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>` );
+
+    return;
+  }
+
 } );
 
 server.listen( port, hostname, () => {

92de01713a1df872188ace2f8258b22c288d9b77
Camilo Romero <camiloaromero23@gmail.com>
2022-02-25 11:08:44 -0500

    Express Serve Static Files

--- nodeJS/coursera-node-course/node-express/index.js
+++ nodeJS/coursera-node-course/node-express/index.js
@@ -1,18 +1,27 @@
 import express from 'express';
-// import http from 'http';
+import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath( import.meta.url );
+const __dirname = dirname( __filename );
 
 const hostname = 'localhost';
 const port = 3000;
 
-const app = express()
+const app = express();
+
+app.use( morgan( 'dev' ) );
+
+app.use( express.static( `${__dirname}/public` ) )
 
 app.use( ( req, res, next ) => {
   console.log( req.headers );
 
-  res.setHeader( 'Content-Type', 'text/html' )
-  res.status( 200 ).send( '<html><body><h1>This is an Express server</h1></body></html>' )
-} )
+  res.setHeader( 'Content-Type', 'text/html' );
+  res.status( 200 ).send( '<html><body><h1>This is an Express server</h1></body></html>' );
+} );
 
 app.listen( port, hostname, () => {
   console.log( `App listening on http://${hostname}:${port}` );
-} )
+} );

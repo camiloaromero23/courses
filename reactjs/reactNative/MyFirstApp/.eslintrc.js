9a1fd3dca575e3d29800f9937cb1ac968c4416c8
Camilo Romero <camilo.romero@kushkipagos.com>
2022-07-14 02:41:44 -0500

    Re-include react-in-jsx-scope

--- reactjs/reactNative/MyFirstApp/.eslintrc.js
+++ reactjs/reactNative/MyFirstApp/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        'react/react-in-jsx-scope': 'off',
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
